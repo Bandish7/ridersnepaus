33b3718284bffc218f83cfd1d6c4b24c5370fcb7
Bandish Basnet <bandishbasnet@gmail.com>
2024-09-15 21:24:50 +1000

    Gallery

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,92 @@
-/** @type {import('tailwindcss').Config} */
+/* eslint-disable @typescript-eslint/no-var-requires */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: [
+     './index.html',
+      './layouts/**/*.html',
+      './content/**/*.md',
+      './content/**/*.html',
+      './src/**/*.ts',
+      './src/**/*.js',
+  ],
+  darkMode: 'class',
   theme: {
-    fontFamily: {
-      'poppins': ["poppins", "sans-serif"],
-      'serif': ['ui-serif', 'Georgia']
-    },
-    extend: {
-      inset:{
-        '20%':'20%'
-      }
-    },
+    screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+      extend: {
+          // sidebar mobile fix
+          maxWidth: {
+              '2xs': '16rem',
+              '8xl': '90rem',
+          },
+          fontFamily: {
+              sans: [
+                  'Inter',
+                  'ui-sans-serif',
+                  'system-ui',
+                  '-apple-system',
+                  'system-ui',
+                  'Segoe UI',
+                  'Roboto',
+                  'Helvetica Neue',
+                  'Arial',
+                  'Noto Sans',
+                  'sans-serif',
+                  'Apple Color Emoji',
+                  'Segoe UI Emoji',
+                  'Segoe UI Symbol',
+                  'Noto Color Emoji',
+              ],
+              body: [
+                  'Inter',
+                  'ui-sans-serif',
+                  'system-ui',
+                  '-apple-system',
+                  'system-ui',
+                  'Segoe UI',
+                  'Roboto',
+                  'Helvetica Neue',
+                  'Arial',
+                  'Noto Sans',
+                  'sans-serif',
+                  'Apple Color Emoji',
+                  'Segoe UI Emoji',
+                  'Segoe UI Symbol',
+                  'Noto Color Emoji',
+              ],
+              mono: [
+                  'ui-monospace',
+                  'SFMono-Regular',
+                  'Menlo',
+                  'Monaco',
+                  'Consolas',
+                  'Liberation Mono',
+                  'Courier New',
+                  'monospace',
+              ],
+          },
+          fontSize: {
+              '2xs': '0.625rem',
+          },
+      },
   },
-  plugins: [],
-}
-
+  variants: {
+      extend: {
+          backgroundColor: ['active'],
+      },
+  }
+  
+};
